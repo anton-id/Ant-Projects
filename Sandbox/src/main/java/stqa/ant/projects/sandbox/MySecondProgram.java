833802f97fb37e5dbe54780ae3b8118902a7ac82
anton.kh <anton.kh@yahoo.com>
2020-04-02 23:25:54 -0600

    Update MySecondProgram.java

--- Sandbox/src/main/java/stqa/ant/projects/sandbox/MySecondProgram.java
+++ Sandbox/src/main/java/stqa/ant/projects/sandbox/MySecondProgram.java
@@ -2,7 +2,7 @@ package stqa.ant.projects.sandbox;
 
 public class MySecondProgram {
 
-public static void main(String[] args) {
-System.out.println("Hello, world!");
-}
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 }
